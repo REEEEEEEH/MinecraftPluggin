43332fb1b4744bf97473fc65f73cfe0c243530de
Vicen <85416824+Vicensk@users.noreply.github.com>
2021-06-15 20:42:02 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+package me.vicensk.vicensk;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPluggin {
+
+   @Override
+   public void onEnable() {
+   System.out.Print('Pluggin working nice my dude')
+   }
+   
+   @Override
+   public void onDisable() {
+   System.out.Print('Pluggin shut down :(')
+   }
+  }
